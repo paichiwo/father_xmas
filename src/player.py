6ea0479f3ee86dae18e39311a248b43ae726adbb
paichiwo <lzerucha@gmail.com>
2024-01-01 00:30:04 +0000

    Implement middle_of_ladder logic to allow player to climb the ladder only if within a certain offset

--- src/player.py
+++ src/player.py
@@ -23,7 +23,8 @@ class Player(pygame.sprite.Sprite):
 
         # Animation index
         self.status = 'walk_right'
-        self.frame_index = 0
+        self.frame_index = 1
+        self.animation_possible = False
 
         self.image = self.frames[self.status][self.frame_index]
         self.rect = self.image.get_rect()
@@ -46,11 +47,14 @@ class Player(pygame.sprite.Sprite):
         self.ladders_rects = [ladder.rect for ladder in self.ladder_group]
 
     def animate(self):
-        self.frame_index += 0.1
-        if self.frame_index >= len(self.frames[self.status]):
-            self.frame_index = 0
+        if self.animation_possible:
+            self.frame_index += 0.1
+            if self.frame_index >= len(self.frames[self.status]):
+                self.frame_index = 0
 
-        self.image = self.frames[self.status][int(self.frame_index)]
+            self.image = self.frames[self.status][int(self.frame_index)]
+        else:
+            self.image = self.frames[self.status][1]
 
     def check_landed(self):
         for i in range(len(self.platform_rects)):
@@ -64,66 +68,74 @@ class Player(pygame.sprite.Sprite):
     def check_climb(self):
         can_climb = False
         climb_down = False
+        middle_of_ladder = False
 
         under = pygame.rect.Rect((self.rect[0], self.rect[1] + self.rect.height),
                                  (self.rect[2], self.rect[3] / 3))
         # pygame.draw.rect(self.screen, 'yellow', under)
+        offset = 5
 
         for ladder in self.ladders_rects:
             if self.rect.colliderect(ladder) and not can_climb:
                 can_climb = True
+                ladder_middle_x = ladder.centerx
+                player_middle_x = under.centerx
+                middle_of_ladder = abs(player_middle_x - ladder_middle_x) <= offset
+
             if under.colliderect(ladder):
                 climb_down = True
+                ladder_middle_x = ladder.centerx
+                player_middle_x = under.centerx
+                middle_of_ladder = abs(player_middle_x - ladder_middle_x) <= offset
 
         if (not can_climb and (not climb_down or self.y_change < 0)) or (
                 self.landed and can_climb and self.y_change > 0 and not climb_down):
             self.climbing = False
 
-        return can_climb, climb_down
+        return can_climb, climb_down, middle_of_ladder
 
     def move(self):
         self.rect.move_ip(self.x_change * self.speed, self.y_change)
         self.bottom = pygame.rect.Rect(self.rect.left, self.rect.bottom, self.rect.width, 3)
 
-    def controls(self, event, can_climb, climbed_down):
+    def controls(self, event, can_climb, climbed_down, middle_of_ladder):
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and not self.climbing:
-                self.x_change = 1
-                self.status = 'walk_right'
-            if event.key == pygame.K_LEFT and not self.climbing:
-                self.x_change = -1
-                self.status = 'walk_left'
-            if event.key == pygame.K_UP:
-                if can_climb:
-                    self.y_change -= 1
-                    self.status = 'climbing'
-                    self.x_change = 0
-                    self.climbing = True
-            if event.key == pygame.K_DOWN:
-                if climbed_down:
-                    self.y_change += 1
-                    self.status = 'climbing'
-                    self.x_change = 0
-                    self.climbing = True
+            self.animation_possible = True
+            if self.animation_possible:
+                if event.key == pygame.K_RIGHT and not self.climbing:
+                    self.x_change = 1
+                    self.status = 'walk_right'
+                if event.key == pygame.K_LEFT and not self.climbing:
+                    self.x_change = -1
+                    self.status = 'walk_left'
+                if event.key == pygame.K_UP and middle_of_ladder:
+                    if can_climb:
+                        self.y_change -= 1
+                        self.x_change = 0
+                        self.climbing = True
+                        self.status = 'climbing'
+                if event.key == pygame.K_DOWN and middle_of_ladder:
+                    if climbed_down:
+                        self.y_change += 1
+                        self.x_change = 0
+                        self.climbing = True
+                        self.status = 'climbing'
 
         if event.type == pygame.KEYUP:
+            self.animation_possible = False
             if event.key == pygame.K_RIGHT:
                 self.x_change = 0
-                self.image = self.frames['walk_right'][1]
             if event.key == pygame.K_LEFT:
                 self.x_change = 0
-                self.image = self.frames['walk_left'][1]
             if event.key == pygame.K_UP:
                 if can_climb:
                     self.y_change = 0
-                    self.image = self.frames['climbing'][1]
                 if self.climbing and self.landed:
                     self.climbing = False
             if event.key == pygame.K_DOWN:
                 if climbed_down:
                     self.y_change = 0
-                self.image = self.frames['climbing'][1]
                 if self.climbing and self.landed:
                     self.climbing = False
 
