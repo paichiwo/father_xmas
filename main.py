b84fed88cbcda631c7fe89831b3a30f0e4e61bde
paichiwo <lzerucha@gmail.com>
2024-01-12 20:10:35 +0000

    Update and draw new level elements. Stop using sdl2 for the window scaling.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import sys
 import pygame
-import pygame._sdl2 as pg_sdl2
 
-from pygame.locals import HIDDEN, SCALED
-from src.config import WIDTH, HEIGHT, SCALE
-from src.camera import CameraGroup
+from pygame.locals import SCALED
+from src.config import WIDTH, HEIGHT
 from src.level import Level
 from src.player import Player
 from src.dashboard import Dashboard
@@ -17,15 +15,9 @@ class Game:
         # Game setup
         pygame.init()
         pygame.display.set_caption('Father Xmas')
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=HIDDEN | SCALED, vsync=1)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=SCALED | pygame.RESIZABLE, vsync=1)
         self.clock = pygame.time.Clock()
         self.fps = 60
-
-        # Scaled window setup
-        self.window = pg_sdl2.Window.from_display_module()
-        self.window.size = (WIDTH * SCALE, HEIGHT * SCALE)
-        self.window.position = pg_sdl2.WINDOWPOS_CENTERED
-        self.window.show()
 
         # Game variables
         self.running = True
@@ -34,24 +26,12 @@ class Game:
         # Sprite groups
         self.player_group = pygame.sprite.GroupSingle()
 
-        # Camera group
-        self.camera_group = CameraGroup()
-
         # Game objects
         self.level = Level(self.screen)
-        self.player = Player(100, 150, self.screen, self.level.ladders_group,
+        self.player = Player(100, 112, self.screen, self.level.ladders_group,
                              self.level.platforms_group, self.player_group)
         self.dashboard = Dashboard(self.screen)
         self.game_over_scene = GameOverScene(self.screen, WIDTH, HEIGHT)
-
-        self.add_to_camera_group()
-
-    def add_to_camera_group(self):
-        self.camera_group.add(
-            self.level.platforms_group,
-            self.level.ladders_group,
-            self.player_group
-        )
 
     def handle_game_events(self, event):
         if event.type == pygame.QUIT:
@@ -62,11 +42,24 @@ class Game:
                 self.reset_game()
 
     def draw_elements(self):
-        self.camera_group.custom_draw(self.player)
+        self.level.platforms_group.draw(self.screen)
+        self.level.ladders_group.draw(self.screen)
+        self.level.walls_group.draw(self.screen)
+        self.level.walls_with_collision_group.draw(self.screen)
+        self.level.decorations_group.draw(self.screen)
+
+        self.player_group.draw(self.screen)
 
     def update_elements(self):
         self.dashboard.update()
-        self.camera_group.update()
+
+        self.level.platforms_group.update()
+        self.level.ladders_group.update()
+        self.level.walls_group.update()
+        self.level.walls_with_collision_group.update()
+        self.level.decorations_group.update()
+
+        self.player_group.update()
 
     def game_over(self):
         return not self.dashboard.game_over
@@ -81,13 +74,9 @@ class Game:
         self.dashboard.reset()
         self.player.reset()
 
-    def test_map(self):
-        img = pygame.image.load('assets/map.png').convert_alpha()
-        img_rect = img.get_rect(topright=(320, 0))
-        self.screen.blit(img, img_rect)
-
     def run(self):
         while True:
+
             self.screen.fill('grey15')
             pygame.key.set_repeat(self.fps)
             can_climb, climbed_down, middle_of_ladder = self.player.check_climb()
@@ -97,15 +86,15 @@ class Game:
                 self.player.controls(event, can_climb, climbed_down, middle_of_ladder)
 
             if self.running:
-                self.test_map()
-                self.draw_elements()
                 self.update_elements()
+                self.draw_elements()
+
                 self.running = self.game_over()
             else:
                 self.game_over_scene_active = True
                 self.show_game_over_screen()
 
-            pygame.display.update()
+            pygame.display.flip()
             self.clock.tick(self.fps)
 
 
