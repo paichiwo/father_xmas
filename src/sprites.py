9903fae2b801bbd5270ec88279435286c5ca1064
paichiwo <lzerucha@gmail.com>
2024-01-12 20:08:48 +0000

    Create sprites.py with all level sprite classes

---
+++ src/sprites.py
@@ -0,0 +1,65 @@
+import pygame
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, length, height, image, screen, group):
+        super().__init__(group)
+
+        # General setup
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.length = length
+        self.height = height
+        self.screen = screen
+
+        # Image & Rect
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(self.x_pos, self.y_pos))
+
+
+class Ladder(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, length, height, screen, group):
+        super().__init__(group)
+
+        # General setup
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.length = length
+        self.height = height
+        self.screen = screen
+
+        # Image & Rect
+        self.image = pygame.image.load('assets/level/images/ladders/ladder.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(x_pos, y_pos))
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, length, height, image, screen, group):
+        super().__init__(group)
+
+        # General setup
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.length = length
+        self.height = height
+        self.screen = screen
+
+        # Image & Rect
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(self.x_pos, self.y_pos))
+
+
+class Decoration(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, length, height, image, screen, group):
+        super().__init__(group)
+
+        # General setup
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.length = length
+        self.height = height
+        self.screen = screen
+
+        # Image & Rect
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(self.x_pos, self.y_pos))
