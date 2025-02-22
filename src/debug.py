b274321884631d4ff502fe65c3056012e92ab48a
paichiwo <lzerucha@gmail.com>
2025-02-22 18:45:12 +0000

    Make debug items align to the left and debug stated to the right

--- src/debug.py
+++ src/debug.py
@@ -8,12 +8,13 @@ class DebugMenu:
         self.clock = clock
 
         self.surf = pygame.Surface((86, HEIGHT))
-        self.surf.fill('GREY')
+        self.surf.fill('BLACK')
         self.surf.set_alpha(200)
         self.rect = self.surf.get_rect(topright=(WIDTH, 0))
 
         self.debug_items = {
-            'FPS': 0
+            'FPS': 0,
+            'UNDER': False
         }
 
         self.item_positions = []
@@ -22,7 +23,7 @@ class DebugMenu:
         self.screen.blit(self.surf, self.rect)
 
     def draw_title(self):
-        title_text = FONT_8.render('DEBUG MENU:', True, 'ORANGE')
+        title_text = FONT_DEBUG.render('DEBUG MENU', True, 'ORANGE')
         title_rect = title_text.get_rect(center=(WIDTH - self.surf.get_width() / 2, 15))
         self.screen.blit(title_text, title_rect)
 
@@ -33,26 +34,27 @@ class DebugMenu:
 
         self.update_fps()
 
-        x = WIDTH - 57
+        padding = 3
+        x_left = self.rect.left + padding
+        x_right = self.rect.right - padding
         y = 30
 
         for item, state in self.debug_items.items():
             item_color = 'ORANGE' if pygame.font.Font.render(FONT_8, item, True, 'YELLOW').get_rect(
-                center=(x, y)).collidepoint(mouse_pos) else 'YELLOW'
+                topleft=(x_left, y)).collidepoint(mouse_pos) else 'YELLOW'
             state_color = 'RED' if not state else 'GREEN'
 
-            item_text = FONT_8.render(item, True, item_color)
-            state_text = FONT_8.render(str(state), True, state_color)
+            item_text = FONT_DEBUG.render(item, True, item_color)
+            item_rect = item_text.get_rect(topleft=(x_left, y))
 
-            item_rect = item_text.get_rect(center=(x, y))
-            state_rect = state_text.get_rect(center=(x + 42, y))
+            state_text = FONT_DEBUG.render(str(state), True, state_color)
+            state_rect = state_text.get_rect(topright=(x_right, y))
 
             self.screen.blit(item_text, item_rect)
             self.screen.blit(state_text, state_rect)
 
             self.item_positions.append((item, item_rect))
             y += 10
-
 
     def update_fps(self):
         self.debug_items['FPS'] = int(self.clock.get_fps())
