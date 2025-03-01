88f286a9ce54cb6921089279fd4c515be805110b
paichiwo <lzerucha@gmail.com>
2025-03-01 12:57:27 +0000

    Refactor enemy

--- src/enemy.py
+++ src/enemy.py
@@ -1,6 +1,6 @@
 import random
-import pygame
 from src.helpers import import_assets
+from src.config import *
 
 class EnemyElf(pygame.sprite.Sprite):
     def __init__(self, pos, direction_x, screen, platformer, path, group):
@@ -9,27 +9,25 @@ class EnemyElf(pygame.sprite.Sprite):
         self.screen = screen
         self.platformer = platformer
 
-        # Image, Rect, Animation
         self.frames = import_assets(path)
-        self.frame_index = 1
-
+        self.frame_index = 0
         self.image = self.frames['walk'][self.frame_index]
         self.rect = self.image.get_rect(midbottom=pos)
-        self.bottom_rect = pygame.rect.Rect(self.rect.left, self.rect.bottom, self.rect.width, 3)
-        self.under_rect = pygame.rect.Rect(0, 0, 0, 0)
+        self.bottom_rect = pygame.rect.Rect()
+        self.under_rect = pygame.rect.Rect()
 
-        # Movement attributes
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
         self.climbing = False
         self.landed = False
+        self.collided_with_wall = False
 
         self.direction.x = direction_x
         self.speed = 50
-        self.random_number = random.choice([0, 1])
+        self.off_screen_max = 100
+        self.climb_decision = random.choice([0, 1]) # 0 = no climbing; 1 = climbing
 
-        # timers
         self.direction_timer = random.randint(2000, 5000)
         self.last_direction_change_time = pygame.time.get_ticks()
 
@@ -37,82 +35,28 @@ class EnemyElf(pygame.sprite.Sprite):
         self.last_off_screen_time = pygame.time.get_ticks()
 
     def animate(self, dt):
-            self.frame_index += 7 * dt
-            if self.frame_index >= len(self.frames['walk']):
-                self.frame_index = 0
+        self.frame_index += 7 * dt
+        if self.frame_index >= len(self.frames['walk']):
+            self.frame_index = 0
+
+        if self.direction.x == -1:
+            self.image = self.frames['walk'][int(self.frame_index)]
+        elif self.direction.x == 1:
+            self.image = pygame.transform.flip(self.frames['walk'][int(self.frame_index)], True, False)
+        elif self.direction.x == 0 and (self.direction.y in [-1, 1]):
+            self.image = self.frames['climb'][int(self.frame_index)]
 
     def move(self, dt):
-        if self.climbing:
-            self.direction.x = 0
+        # if self.climbing:
+        #     self.direction.x = 0
 
         # horizontal
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.x = round(self.pos.x)
 
-        # horizontal
+        # vertical
         self.pos.y += self.direction.y * self.speed * dt
         self.rect.y = round(self.pos.y)
-
-    def direction_change(self):
-        can_climb_up, can_climb_down, middle_of_ladder = self.check_climb()
-        current_time = pygame.time.get_ticks()
-        elapsed_time = current_time - self.last_direction_change_time
-
-        # horizontal
-        if not self.climbing and self.landed:
-            if elapsed_time >= self.direction_timer or self.pos.x > 450 or self.pos.x < -120:
-                self.direction.y = 0
-                self.direction.x *= -1
-                self.random_number = 0
-                self.last_direction_change_time = current_time
-                self.direction_timer = random.randint(2000, 5000)
-
-        # vertical
-        if can_climb_up and middle_of_ladder and self.random_number == 0:
-            print('can climb up')
-            if self.direction.y == 0:  # Not already climbing
-                self.direction.y = -1  # Climb up
-                self.direction.x = 0
-                self.climbing = True
-                self.random_number = random.choice([0, 1, 1])
-
-        elif can_climb_down and middle_of_ladder and self.random_number == 0:
-            print('can climb down')
-            if self.direction.y == 0:  # Not already climbing
-                self.direction.y = 1  # Climb down
-                self.direction.x = 0
-                self.climbing = True
-                self.random_number = random.choice([0, 1])
-
-        else:
-            if self.climbing and self.landed:
-                self.direction.y = 0
-                self.climbing = False
-                self.direction.x = self.random_number
-                self.random_number = random.choice([-1, 1])
-
-    def collisions(self):
-        # platform collision
-        self.landed = False
-        for platform in self.platformer.platforms_group:
-            if self.bottom_rect.colliderect(platform.rect):
-                self.landed = True
-                if not self.climbing:
-                    self.direction.y = 0
-                    self.rect.bottom = platform.rect[1]
-
-        # wall collision
-        for wall in self.platformer.collision_walls:
-
-            if self.rect.colliderect(wall.rect):
-                if self.direction.x > 0:
-                    self.direction.x = -1
-                else:
-                    self.direction.x = 1
-
-                self.pos.x = self.rect[0]
-
-        self.bottom_rect = pygame.rect.Rect(self.rect.left, self.rect.bottom, self.rect.width, 3)
 
     def check_climb(self):
         can_climb_up = False
@@ -143,16 +87,78 @@ class EnemyElf(pygame.sprite.Sprite):
 
         return can_climb_up, can_climb_down, middle_of_ladder
 
-    def update(self, dt):
-        # anim frames to use
-        if self.direction.x == -1:
-            self.image = self.frames['walk'][int(self.frame_index)]
-        elif self.direction.x == 1:
-            self.image = pygame.transform.flip(self.frames['walk'][int(self.frame_index)], True, False)
-        elif self.direction.x == 0 and (self.direction.y == -1 or self.direction.y == 1):
-            self.image = self.frames['climb'][int(self.frame_index)]
+    def direction_change(self):
+        can_climb_up, can_climb_down, middle_of_ladder = self.check_climb()
+        elapsed_time = pygame.time.get_ticks() - self.last_direction_change_time
 
-        self.move(dt)
+        # horizontal movement
+        if (not self.climbing and self.landed or
+                (self.pos.x > WIDTH + self.off_screen_max and not self.landed) or
+                (self.pos.x < -self.off_screen_max and not self.landed)):
+            if elapsed_time >= self.direction_timer or self.collided_with_wall:
+                self.direction.y = 0
+                self.direction.x *= -1
+                self.direction_timer = random.randint(2000, 5000)
+                self.last_direction_change_time = pygame.time.get_ticks()
+                self.climb_decision = 1
+
+        # if not self.climbing and self.landed:
+        #     if elapsed_time >= self.direction_timer or self.pos.x > 450 or self.pos.x < -120 or self.collided_with_wall:
+        #         self.direction.y = 0
+        #         self.direction.x *= -1
+        #         self.climb_decision = 1
+        #         self.last_direction_change_time = pygame.time.get_ticks()
+        #         self.direction_timer = random.randint(2000, 5000)
+
+        # vertical movement
+        # GOING UP
+        if can_climb_up and middle_of_ladder and self.climb_decision == 1:
+            if self.direction.y == 0:
+                self.direction.y = -1
+                self.direction.x = 0
+                self.climbing = True
+
+                self.climb_decision = random.choice([0, 1])
+
+        # GOING DOWN
+        elif can_climb_down and middle_of_ladder:
+            if self.direction.y == 0:
+                self.direction.y = 1
+                self.direction.x = 0
+                self.climbing = True
+
+                self.climb_decision = random.choice([0, 1])
+
+        # GOING UP OR DOWN BUT LANDED ON THE PLATFORM
+        else:
+            if self.climbing and self.landed and self.climb_decision == 1:
+                self.direction.y = 0
+                self.climbing = False
+                self.direction.x = random.choice([-1, 1])
+
+        print('climbing', self.climbing)
+
+    def collisions_with_platforms(self):
+        self.landed = False
+        for platform in self.platformer.platforms_group:
+            if self.bottom_rect.colliderect(platform.rect):
+                self.landed = True
+                if not self.climbing:
+                    self.direction.y = 0
+                    self.rect.bottom = platform.rect[1]
+        print('landed', self.landed)
+
+        self.bottom_rect = pygame.rect.Rect(self.rect.left, self.rect.bottom, self.rect.width, 3)
+
+    def collisions_with_walls(self):
+        self.collided_with_wall = False
+        for wall in self.platformer.collision_walls:
+            if self.rect.colliderect(wall.rect):
+                self.collided_with_wall = True
+
+    def update(self, dt):
         self.animate(dt)
+        self.move(dt)
+        self.collisions_with_platforms()
+        self.collisions_with_walls()
         self.direction_change()
-        self.collisions()
