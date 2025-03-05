b118e143d780e4521d88184874ea77d88d56f03b
paichiwo <lzerucha@gmail.com>
2025-03-05 21:44:24 +0000

    Add tiles.png and tileset from Tiled

---
+++ data/levels/father_xmas_tiles.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles.png" tilewidth="16" tileheight="16" tilecount="320" columns="20">
+ <image source="tiles.png" width="320" height="258"/>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="80">
+  <animation>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="81" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="132">
+  <animation>
+   <frame tileid="132" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="136" duration="100"/>
+   <frame tileid="138" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="133">
+  <animation>
+   <frame tileid="133" duration="100"/>
+   <frame tileid="135" duration="100"/>
+   <frame tileid="137" duration="100"/>
+   <frame tileid="139" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
