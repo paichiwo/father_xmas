6dbdad128d4da57a28ba50ef8824a8e2ea64bf86
paichiwo <lzerucha@gmail.com>
2025-03-07 19:51:03 +0000

    Update Snowflake class

--- src/sprites.py
+++ src/sprites.py
@@ -1,7 +1,5 @@
-import random
-import pygame
-from src.config import WHITE, TILE_SIZE
-
+from random import randint, choice
+from src.config import *
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, group):
@@ -42,15 +40,17 @@ class Sleigh(Sprite):
 
 
 class Snowflake:
-    def __init__(self, x, y, screen, snow_boundary):
-        self.pos = pygame.Vector2(x, y)
+    def __init__(self, pos, screen, snow_boundary):
         self.screen = screen
         self.snow_boundary = snow_boundary
-        self.size = random.randint(1, 2)
-        self.speed = random.choice([20, 30, 40, 50])
+
+        self.pos = pygame.Vector2(pos)
+        self.size = randint(1, 2)
+        self.speed = choice([20, 30, 40, 50])
+        self.color = f'grey{randint(70, 90)}'
 
     def draw(self):
-        pygame.draw.circle(self.screen, WHITE, (int(self.pos.x), int(self.pos.y)), self.size)
+        pygame.draw.circle(self.screen, self.color, self.pos, self.size)
 
     def update(self, dt):
         self.pos.y += self.speed * dt
@@ -58,7 +58,7 @@ class Snowflake:
         # Respawn at top when it falls below boundary
         if self.pos.y > self.snow_boundary.bottom:
             self.pos.y = self.snow_boundary.top
-            self.pos.x = random.randint(self.snow_boundary.left, self.snow_boundary.right)
+            self.pos.x = randint(self.snow_boundary.left, self.snow_boundary.right)
 
         # Ensure flakes stay within horizontal bounds
         if self.pos.x < self.snow_boundary.left:
