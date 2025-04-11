a54da69a7708f49458438300787d03c6538c528e
paichiwo <lzerucha@gmail.com>
2025-04-11 15:15:37 +0100

    Create basic level 2

--- src/level_2.py
+++ src/level_2.py
@@ -1,17 +1,39 @@
+from random import randint, choice
 from src.config import *
+from src.helpers import import_images
+from src.sprites import Sprite, AnimatedSprite, Snowflake, Sleigh
+from src.player import Player
+from src.enemy import EnemyElf
+from src.timer import Timer
+from pytmx import load_pygame
 
 
 class LevelTwo:
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.Surface((20, 20))
-        pygame.draw.circle(self.image, 'blue', (self.image.get_width() // 2, self.image.get_height() // 2), 10)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (30, 30)
+        self.level_won = False
 
-    def draw(self):
-        self.screen.blit(self.image, self.rect)
-        pygame.draw.rect(self.screen, 'red', self.rect, 1)
+        # sprite groups
+        self.all_sprites = pygame.sprite.Group()
 
-    def update(self):
-        self.rect.x += 1
+        # snow
+        self.snow_images = import_images(PATHS['snow'])
+
+        self.create_room(load_pygame('data/levels/level_2.tmx'))
+
+    def create_room(self, tmx_map):
+        """Creates the current room by adding objects to sprite groups."""
+
+        for x, y, surf in tmx_map.get_layer_by_name('letter').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, [self.all_sprites])
+
+        for x, y, gid in tmx_map.get_layer_by_name('animations'):
+            props = tmx_map.get_tile_properties_by_gid(gid)
+            if props and 'frames' in props:
+                frames = [tmx_map.get_tile_image_by_gid(frame.gid) for frame in props['frames']]
+                AnimatedSprite((x * TILE_SIZE, y * TILE_SIZE), frames, [self.all_sprites])
+
+    def update(self, dt):
+        self.all_sprites.update(dt)
+        self.all_sprites.draw(self.screen)
+        print(len(self.all_sprites))
